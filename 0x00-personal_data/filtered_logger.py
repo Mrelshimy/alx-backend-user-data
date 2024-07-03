76e87825724915259e217d469cd4a245a07576b8
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-03 20:36:24 +0300

    task0_annotations

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,8 @@
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: list, redaction: str,
+                 message: str, separator: str) -> str:
     """ Function to return log message obfuscated """
     for field in fields:
         message = re.sub(field+'=.*?'+separator,
