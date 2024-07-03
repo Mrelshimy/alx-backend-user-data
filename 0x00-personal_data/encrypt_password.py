48370393624511a4e3452f88af252e530f704e37
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-03 22:47:34 +0300

    task6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,9 @@ def hash_password(password: str) -> bytes:
     """ method to has data using bcrypt"""
     password = password.encode()
     return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+def is_valid(hasehd_password: bytes, password: str) -> bool:
+    """ function to validate that hashed pass
+     matches password """
+    return bcrypt.checkpw(hasehd_password, password.encode())
