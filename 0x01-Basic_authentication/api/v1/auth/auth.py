0c87f222f870c26e0a48947e84b2df59d070b0ac
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-09 23:30:21 +0300

    task3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Auth module for the API """
+from flask import request
+from typing import List
+
+
+class Auth:
+    """ Basic Auth class """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user
+        """
+        return None
