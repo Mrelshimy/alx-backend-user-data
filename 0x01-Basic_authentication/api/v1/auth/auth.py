05201e1ea14a0aa3f3a88fdda38697d53fb52797
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-10 00:05:36 +0300

    task5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,15 +7,12 @@ from typing import List, TypeVar
 class Auth:
     """ Basic Auth class """
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require_auth
-        """
-        return False
-
     def authorization_header(self, request=None) -> str:
         """ authorization_header
         """
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current_user
@@ -27,8 +24,8 @@ class Auth:
         """
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
-        if path[-1] != '/':
-            path += '/'
+        if path[-1] != "/":
+            path += "/"
         if path in excluded_paths:
             return False
         return True
