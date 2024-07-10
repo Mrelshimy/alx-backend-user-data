0ad16a606797dabab2a86c9c2e8ae8acb61aaee7
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-10 12:32:12 +0300

    task7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,25 +8,13 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic Auth class """
 
-    def authorization_header(self, request=None) -> str:
-        """ authorization_header
-        """
-        if request is None:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Extract Authorization header data """
+
+        if authorization_header is None or\
+            not isinstance(authorization_header, str) or\
+                authorization_header[0:6] != "Basic ":
             return None
-        return request.headers.get('Authorization')
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """ current_user
-        """
-        return None
-
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require_auth
-        """
-        if path is None or excluded_paths is None or excluded_paths == []:
-            return True
-        if path[-1] != "/":
-            path += "/"
-        if path in excluded_paths:
-            return False
-        return True
+        return authorization_header[6:]
