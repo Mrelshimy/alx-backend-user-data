f3a64d0d8d72ec15b8aad8213959e7c59d306686
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-16 15:42:31 +0300

    task14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -41,11 +41,11 @@ def login() -> str:
 @app.route("/sessions", methods=['DELETE'])
 def logout() -> None:
     session_id = request.cookies.get('session_id')
-    try:
-        user = AUTH.get_user_from_session_id(session_id)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
         AUTH.destroy_session(user.id)
         return redirect('/')
-    except Exception:
+    else:
         abort(403)
 
 
