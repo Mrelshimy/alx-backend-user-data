c8e3fdd238c68548fa4e2f748e3664b9c5a3d36e
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-16 12:04:35 +0300

    bugfix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Auth model module """
 import bcrypt
+from typing import Union
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
@@ -19,12 +20,11 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email, password):
-        """ Register a new user """
+    def register_user(self, email: str, password: str) -> Union[None, User]:
+        """ Register new user """
         try:
             self._db.find_user_by(email=email)
         except NoResultFound:
-            user = self._db.add_user(email, _hash_password(password))
-            return user
+            return self._db.add_user(email, _hash_password(password))
         else:
             raise ValueError('User {} already exists'.format(email))
