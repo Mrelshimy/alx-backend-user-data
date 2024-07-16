1ad61504617b440698317e7f3dce5399ebf27e75
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-16 11:32:00 +0300

    bugfix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,7 +49,7 @@ class DB:
         """ Update user method """
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
-            if key not in user.__dict__:
+            if not hasattr(user, key):
                 raise ValueError
             setattr(user, key, value)
         self._session.commit()
