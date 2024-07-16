b203e78279cf53c06d52d2331818e4472d0152c7
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-16 12:02:37 +0300

    bugfix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,4 +27,4 @@ class Auth:
             user = self._db.add_user(email, _hash_password(password))
             return user
         else:
-            raise ValueError(f"User {email} already exists")
+            raise ValueError('User {} already exists'.format(email))
