52b3a8bc593e79997e103ef1290728b0d0019757
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-18 20:53:40 +0300

    bugfix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,15 +49,16 @@ def logout():
     return redirect("/")
 
 
-@app.route("/profile", methods=["GET"])
+@app.route("/profile", methods=["GET"], strict_slashes=False)
 def profile() -> str:
-    """ Get profile method to validate user availability"""
-    session_id = request.cookies.get("session_id", None)
+    """
+    Return a user's email based on session_id in the received cookies
+    """
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
-    if user is None or session_id is None:
-        abort(403)
-    else:
-        return jsonify({"email": user.email}), 200
+    if user:
+        return jsonify({"email": f"{user.email}"}), 200
+    abort(403)
 
 
 if __name__ == "__main__":
